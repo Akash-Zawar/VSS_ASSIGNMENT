b53c0e930b1dcc73854ef2a5b6d9ade3d42e9a1d
pravin <96168062+Akash-Asink@users.noreply.github.com>
2023-04-17 14:34:22 +0530

    added js

---
+++ asset/js/script.js
@@ -0,0 +1,89 @@
+/** compare password and confirmpassword
+ * Search for uppercase, lowercase, numerical and special character
+ * check for length of string
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    const _submitButton = document.getElementById("submitButton");
+    console.log(_submitButton);
+    _submitButton.addEventListener("click", () => {
+      const _passwordElem = document.getElementById("pass1");
+      const _confirmPasswordElem = document.getElementById("pass2");
+  
+      const _password = _passwordElem.value;
+      const _confirmPassword = _confirmPasswordElem.value;
+  
+      console.log(_password);
+      console.log(_confirmPassword);
+      checkLength(_password);
+      compare(_password,_confirmPassword);
+      checkUpperCase(_password);
+      checkLowerCase(_password);
+      checkNumber(_password);
+      checkSpecialCharacter(_password);   
+  
+    });
+  });
+  
+  function compare(a,b){
+      if (a==b) {
+          console.log("Passwords are same"); 
+          return true;
+      }
+       console.log("Error, password and confirm password must be same");
+       return false;
+      }
+  
+  function checkLength(a){
+      if(a.length>=8){
+          console.log("Minimun length satisfied"); 
+          return true;
+      }
+      console.log("Password must contain atleast 8 characters");
+      return false;
+  }
+  
+  function checkUpperCase(a){
+      const match = a.search(/[A-Z]/) == '-1' ? false : true;
+      if (match) {
+          console.log("contains Upper case letter");
+      return true;
+      }
+      console.log("must contain a Upper case Letter");
+      return false;
+  }
+  
+  function checkLowerCase(a){
+      const match = a.search(/[a-z]/) == '-1' ? false : true;
+      if (match) {
+          console.log("contains Lower case letter");
+      return true;
+      }
+      console.log("must contain a Lower case Letter");
+      return false;
+  }
+  
+  function checkNumber(a){
+      const match = a.search(/[0-9]/) == '-1' ? false : true;
+      if (match) {
+          console.log("contains Number");
+      return true;
+      }
+      console.log("must contain Number");
+      return false;
+  }
+  
+  // /!@#\$%\^\&*\)\(+=._-]+$/ regex for checking special character
+  
+  function checkSpecialCharacter(a){
+      const regex = /[!@#$%^&*(),.?":{}|<>]/;
+      const match = a.search(regex) == '-1' ? false : true;
+      if (match) {
+          console.log("contains Special Character");
+      return true;
+      }
+      console.log("must contain a Special Character");
+      return false;
+  }
+  
